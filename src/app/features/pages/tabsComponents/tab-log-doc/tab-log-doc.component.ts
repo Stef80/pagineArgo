24f255663c11de19df3fa533bc74d43185cfd678
Stef80 <mide646@gmail.com>
2020-02-24 19:27:53 +0100

    aggiunta tabella tab-log

--- src/app/features/pages/tabsComponents/tab-log-doc/tab-log-doc.component.ts
+++ src/app/features/pages/tabsComponents/tab-log-doc/tab-log-doc.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-tab-log-doc',
-  templateUrl: './tab-log-doc.component.html',
-  styleUrls: ['./tab-log-doc.component.scss']
+  selector: "app-tab-log-doc",
+  templateUrl: "./tab-log-doc.component.html",
+  styleUrls: ["./tab-log-doc.component.scss"]
 })
 export class TabLogDocComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
-  }
-
+  path: string = "http://localhost:8080/testdoc/log/32831";
 }
