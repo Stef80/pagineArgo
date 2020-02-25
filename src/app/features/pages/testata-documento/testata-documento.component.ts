753aa80fdcf8f8d702b45b5afc8da67447168ba5
Stef80 <mide646@gmail.com>
2020-02-25 10:07:06 +0100

    revert di ieri

--- src/app/features/pages/testata-documento/testata-documento.component.ts
+++ src/app/features/pages/testata-documento/testata-documento.component.ts
@@ -3,6 +3,8 @@ import { TabPaneItem } from "../tab/tab-pane-item";
 import { TabItem } from "../tab/tab-item";
 import { TabPanesService } from "../../services/tab-panes.service";
 import { TabItemsService } from "../../services/tab-items.service";
+import { HttpClient } from "@angular/common/http";
+import { Documento } from "../../models/documento";
 
 @Component({
   selector: "app-testata-documento",
@@ -12,14 +14,27 @@ import { TabItemsService } from "../../services/tab-items.service";
 export class TestataDocumentoComponent implements OnInit {
   public tabPaneItems: TabPaneItem[];
   public tabItems: TabItem[];
+  public doc: Documento;
 
   constructor(
     private tabPaneService: TabPanesService,
-    private tabItemService: TabItemsService
+    private tabItemService: TabItemsService,
+    private http: HttpClient
   ) {}
 
   ngOnInit() {
     this.tabItems = this.tabItemService.getTabItem();
     this.tabPaneItems = this.tabPaneService.getTabPanes();
+    this.getDoc("32831");
+  }
+
+  public getDoc(idDoc: string) {
+    return this.http
+      .get<Documento>("http://localhost:8080/testdoc/" + idDoc)
+      .subscribe(risp => {
+        console.log("questo e risp", risp);
+        this.doc = risp;
+        console.log("questo e doc", this.doc);
+      });
   }
 }
