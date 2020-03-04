180d914b98ba198cb1c38030782197b9e6975c44
fra.1234 <francesco.lillo@nextre.it>
2020-03-04 12:02:03 +0100

    Modifica sidebar

--- src/app/features/pages/side-bar/side-bar.component.ts
+++ src/app/features/pages/side-bar/side-bar.component.ts
@@ -1,18 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-import { ChiamataComponent } from '../chiamata/chiamata.component';
+
 
 @Component({
   selector: 'app-side-bar',
   templateUrl: './side-bar.component.html',
-  styleUrls: ['./side-bar.component.scss']
+  styleUrls: ['./side-bar.component.css']
 })
 export class SideBarComponent implements OnInit {
-  chiamata: ChiamataComponent
+  
   constructor() { }
 
   ngOnInit() {
   }
-  show4: boolean = false;
+  
 
  
   }
